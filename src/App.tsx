3f79c9e12669b00a66b49cae13a0036fc2267df2
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-26 17:38:22 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,24 @@
 import * as React from 'react'
 import './App.css'
 
-import { PerformanceList, getPerformanceListItems } from './wireframes/performance-list'
-import { PerformanceDetail, getPerformanceDetailData } from './wireframes/performance-detail'
-import { AddPerformance } from './wireframes/add-performance'
+import { Container } from 'semantic-ui-react'
+import { Root } from './pages/Root'
+import { Navigation } from './components/navigation/Navigation'
 
-const WIREFRAMES = {
-  'PerformanceList': () => {
-    return (
-      <PerformanceList {...getPerformanceListItems()} />
-    )
-  },
-  'PerformanceDetail': () => {
-    return (
-      <PerformanceDetail {...getPerformanceDetailData()} />
-    )
-  },
-  'AddPerformance': () => (
-    <AddPerformance />
-  )
-}
-
-interface AppState {
-  wireframeName: string
-}
-class App extends React.Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props)
-
-    this.state = {
-      wireframeName: 'PerformanceDetail'
-    }
-  }
-
-  selectWireframe = (name: string) => {
-    this.setState({ wireframeName: name })
-  }
-
-  renderWireframeNav(): JSX.Element {
-    return (
-      <ul>
-        {Object.keys(WIREFRAMES).map(name => {
-          return (
-            <a 
-              key={name} 
-              onClick={(evt: React.SyntheticEvent<HTMLAnchorElement>) => {
-                evt.preventDefault()
-                evt.stopPropagation()
-                this.selectWireframe(name)
-              }}
-              style={{marginRight: '1rem'}}
-            >{name}
-            </a>
-          )
-        })}
-      </ul>
-    )
-  }
-
-  renderCurrentWireframe(): JSX.Element  {
-    return WIREFRAMES[this.state.wireframeName]()
-  }
-
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        Wireframes
-        {this.renderWireframeNav()}
-        {this.renderCurrentWireframe()}
+        <Navigation 
+          links={[
+            {to: '/', title: 'Home'},
+            {to: '/performances', title: 'Upcoming Performances'},
+            {to: '/add-performance', title: 'Add Performance'}
+          ]}
+        />
+        <Container>
+          <Root />
+        </Container>
       </div>
     )
   }
