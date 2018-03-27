94bc5ef0afb3c7e5627360a028f9b374fd31ecb4
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-27 11:29:53 -0400

    created user components

---
+++ src/components/user/Navigation.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+
+import { Navigation } from '../UI/navigation/Navigation'
+
+interface UserNavigationProps {
+  isAuthenticated: boolean
+  isAdmin: boolean
+}
+
+interface UserLink {
+  to: string
+  title: string
+}
+const makeLinks = (args: UserNavigationProps = {isAuthenticated: false, isAdmin: false}): UserLink[] => {
+  let links: UserLink[] = [
+    // basic routes
+    {to: '/', title: 'Home'}
+  ]
+
+  if (args.isAuthenticated === true) {
+    links.push(
+      {to: '/performances', title: 'Upcoming Performances'}
+    )
+    if (args.isAdmin === true) {
+      links.push(
+        {to: '/add-performance', title: 'Add Performance'}
+      )
+    }
+  } else {
+    links.push(
+      {to: '/signin', title: 'Sign In'},
+      {to: '/signup', title: 'Sign Up'}
+    )
+  }
+
+  return links
+}
+
+export const UserNavigation = (props: UserNavigationProps): JSX.Element => {
+  const links = makeLinks(props)
+
+  return (
+    <Navigation links={links}/>
+  )
+}
---
+++ src/components/user/SignInForm.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react'
+
+import { Form } from 'semantic-ui-react'
+
+interface SignInFormProps { }
+interface SignInFormState { 
+  email: string
+  password: string
+  pristine: boolean
+}
+const NEW_SIGNIN_STATE: SignInFormState = {
+  email: '',
+  password: '',
+  pristine: true
+}
+export class SignInForm extends React.Component<SignInFormProps, SignInFormState> {
+  constructor(props: SignInFormProps) {
+    super(props)
+
+    this.state = {
+      ...NEW_SIGNIN_STATE
+    }
+  }
+
+  get isValid(): boolean {
+    const { email, password, pristine } = this.state
+    return !pristine && email !== '' && password !== ''
+  }
+
+  onChange = (evt: React.SyntheticEvent<HTMLInputElement>, data: { name: string, value: string} ) => {
+    const { name, value } = data
+    if (name === 'email') {
+      this.setState({ email: value, pristine: false })
+    } else if (name === 'password') {
+      this.setState({ password: value, pristine: false })
+    }
+  }
+
+  resetForm = (evt: React.SyntheticEvent<HTMLButtonElement>) => {
+    evt.preventDefault()
+    evt.stopPropagation()
+
+    this.setState({
+      ...NEW_SIGNIN_STATE
+    })
+  }
+
+  submit = (evt: React.SyntheticEvent<HTMLFormElement | HTMLButtonElement>) => {
+    evt.preventDefault()
+    evt.stopPropagation()
+
+    if (this.isValid) {
+      console.dir(this.state)
+    }
+  }
+
+  render() {
+    return (
+      <Form>
+        <Form.Input 
+          type="email"
+          name="email"
+          value={this.state.email}
+          onChange={this.onChange}
+        />
+        <Form.Input 
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.onChange}
+        />
+        <Form.Group>
+          <Form.Button 
+            color="blue" 
+            disabled={!this.isValid} 
+            onClick={this.submit}
+          >
+            Sign In
+          </Form.Button>
+          <Form.Button basic color="blue" onClick={this.resetForm}>
+            Clear Form
+          </Form.Button>
+        </Form.Group>
+      </Form>
+    )
+  }
+}
