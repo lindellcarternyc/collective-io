7d65feaf62119d4304961f7857297e2b7d0db96e
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-28 13:56:10 -0400

    updated signinpage to use redux

--- src/pages/user/SignInPage.tsx
+++ src/pages/user/SignInPage.tsx
@@ -2,11 +2,34 @@ import * as React from 'react'
 
 import { Layout } from '../../components/UI/Layout'
 import { SignInForm } from '../../components/user/SignInForm'
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+import { StoreState } from '../../redux/state'
+import { signIn } from '../../redux/auth/actions'
 
-export const SignInPage = (): JSX.Element => {
+interface SignInPageProps {
+  signin: (email: string, password: string) => void
+}
+const SignInPageComponent = (props: SignInPageProps): JSX.Element => {
   return (
     <Layout title="Sign In">
-      <SignInForm />
+      <SignInForm signIn={props.signin} />
     </Layout>
   )
 }
+
+interface DispatchProps {
+  signin: (email: string, password: string) => void
+}
+const mapDispatchToProps = (dispatch: Dispatch<StoreState>): DispatchProps => {
+  return {
+    signin: (email: string, password: string) => {
+      dispatch(signIn(email, password))
+    }
+  }
+}
+
+export const SignInPage = connect(
+  null,
+  mapDispatchToProps
+)(SignInPageComponent)
