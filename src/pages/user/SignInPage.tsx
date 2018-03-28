60850aacdbd4012c56ba18103af5e673f463ce01
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-28 15:12:29 -0400

    added redirects from signin page

--- src/pages/user/SignInPage.tsx
+++ src/pages/user/SignInPage.tsx
@@ -1,21 +1,39 @@
 import * as React from 'react'
-
-import { Layout } from '../../components/UI/Layout'
-import { SignInForm } from '../../components/user/SignInForm'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
+import { Redirect } from 'react-router-dom'
+
 import { StoreState } from '../../redux/state'
 import { signIn } from '../../redux/auth/actions'
 
+import { Layout } from '../../components/UI/Layout'
+import { SignInForm } from '../../components/user/SignInForm'
+
+import { UserType } from '../../constants/user'
+
 interface SignInPageProps {
   signin: (email: string, password: string) => void
+  user: UserType | null
 }
 const SignInPageComponent = (props: SignInPageProps): JSX.Element => {
   return (
-    <Layout title="Sign In">
-      <SignInForm signIn={props.signin} />
-    </Layout>
+    props.user !== null
+    ? <Redirect to="/" />
+    : (
+      <Layout title="Sign In">
+        <SignInForm signIn={props.signin} />
+      </Layout>
+    )
   )
+}
+
+interface StateProps {
+  user: UserType | null
+}
+const mapStateToProps = (state: StoreState): StateProps => {
+  return {
+    user: state.auth.user
+  }
 }
 
 interface DispatchProps {
@@ -30,6 +48,6 @@ const mapDispatchToProps = (dispatch: Dispatch<StoreState>): DispatchProps => {
 }
 
 export const SignInPage = connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(SignInPageComponent)
