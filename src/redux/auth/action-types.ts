e38d02a0a86d3c0332ff6fe044f7476a7b97f29c
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-28 13:48:45 -0400

    created auth action-types

---
+++ src/redux/auth/action-types.ts
@@ -0,0 +1,28 @@
+import { UserType } from '../../constants/user'
+import { AuthError } from './constants'
+
+export const SIGN_IN_START_TYPE = 'SIGN_IN_START_TYPE'
+export type SIGN_IN_START_TYPE = typeof SIGN_IN_START_TYPE
+export interface SignInStartAction {
+  type: SIGN_IN_START_TYPE
+}
+
+export const SIGN_IN_SUCCESS_TYPE = 'SIGN_IN_SUCCESS_TYPE'
+export type SIGN_IN_SUCCESS_TYPE = typeof SIGN_IN_SUCCESS_TYPE
+export interface SignInSuccessAction {
+  type: SIGN_IN_SUCCESS_TYPE,
+  payload: UserType
+}
+
+export const SIGN_IN_FAILURE_TYPE = 'SIGN_IN_FAILURE_TYPE'
+export type SIGN_IN_FAILURE_TYPE = typeof SIGN_IN_FAILURE_TYPE
+export interface SignInFailureAction {
+  type: SIGN_IN_FAILURE_TYPE,
+  payload: AuthError
+}
+
+export const SIGN_OUT_TYPE = 'SIGN_OUT_ACTION'
+export type SIGN_OUT_TYPE = typeof SIGN_OUT_TYPE
+export interface SignOutAction {
+  type: SIGN_OUT_TYPE
+}
