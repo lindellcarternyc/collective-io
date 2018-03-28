770043d407d984cd0bfd683c672c777ba9c13f95
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-28 13:57:25 -0400

    updated user mock database

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,6 +1,6 @@
 import { UserType } from '../constants/user'
 
-export const USERS: { [id: string]: UserType } = {
+const USERS: { [id: string]: UserType } = {
   'a': {
     id: 'a',
     email: 'email1',
@@ -14,10 +14,41 @@ export const USERS: { [id: string]: UserType } = {
     firstName: 'Jose',
     lastName: 'Heredia',
     password: 'password2'
+  },
+  'c': {
+    id: 'c',
+    email: 'lindellcarternyc@gmail.com',
+    firstName: 'Lindell',
+    lastName: 'Carter',
+    password: 'password1'
   }
 }
 
-export const getAllUsers = () => USERS
-export const getUserById = (id: string) => {
-  return USERS[id]
+export const getAllUsers = (): Promise<UserType[]> => {
+  return new Promise((resolve, reject) => {
+    const users = Object.keys(USERS).map(id => USERS[id])
+    resolve(users)
+  })
+}
+
+export const getUserWithEmailAndPassword = (email: string, password: string): Promise<UserType> => {
+  return new Promise((resolve, reject) => {
+    getAllUsers().then(
+      users => {
+        const withEmail = users.filter(user => user.email === email)
+        if (withEmail.length !== 1) {
+          reject('Email error')
+        } else {
+          const user = withEmail[0]
+          if (user.password === password) {
+            resolve(user)
+          } else {
+            reject('Invalid password')
+          }
+        }
+      }
+    ).catch(err => {
+      throw new Error(err)
+    })
+  })
 }
