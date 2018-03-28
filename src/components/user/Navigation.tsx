4f0589213d51a5217fc74e86d81b72031d20aa77
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-28 13:59:33 -0400

    refactored user nav

--- src/components/user/Navigation.tsx
+++ src/components/user/Navigation.tsx
@@ -5,23 +5,24 @@ import { Navigation } from '../UI/navigation/Navigation'
 interface UserNavigationProps {
   isAuthenticated: boolean
   isAdmin: boolean
+  signOut: () => void
 }
-
 interface UserLink {
   to: string
   title: string
 }
-const makeLinks = (args: UserNavigationProps = {isAuthenticated: false, isAdmin: false}): UserLink[] => {
+// interface 
+const makeLinks = (isAuthenticated: boolean =  false, isAdmin: boolean =  false): UserLink[] => {
   let links: UserLink[] = [
     // basic routes
     {to: '/', title: 'Home'}
   ]
 
-  if (args.isAuthenticated === true) {
+  if (isAuthenticated === true) {
     links.push(
       {to: '/performances', title: 'Upcoming Performances'}
     )
-    if (args.isAdmin === true) {
+    if (isAdmin === true) {
       links.push(
         {to: '/add-performance', title: 'Add Performance'}
       )
@@ -36,9 +37,44 @@ const makeLinks = (args: UserNavigationProps = {isAuthenticated: false, isAdmin:
   return links
 }
 
-export const UserNavigation = (props: UserNavigationProps): JSX.Element => {
-  const links = makeLinks(props)
+const UserNavigationComponent = (props: UserNavigationProps): JSX.Element => {
+  const links = makeLinks(props.isAuthenticated, props.isAdmin)
   return (
-    <Navigation links={links} logout={props.isAuthenticated} />
+    <Navigation 
+      links={links}
+      isAuthenticated={props.isAuthenticated}
+      signOut={props.signOut}
+    />
   )
 }
+
+import { StoreState } from '../../redux/state'
+import { connect } from 'react-redux'
+interface StateProps {
+  isAuthenticated: boolean
+  isAdmin: boolean
+}
+const mapStateToProps = (state: StoreState): StateProps => {
+  const { auth } = state
+  return {
+    isAuthenticated: auth.user !== null,
+    isAdmin: false
+  }
+}
+
+import { Dispatch } from 'redux'
+import { signOut } from '../../redux/auth/actions' 
+interface DispatchProps {
+  signOut: () => void
+}
+const mapDispatchToProps = (dispatch: Dispatch<StoreState>): DispatchProps => {
+  return {
+    signOut: () => {
+      dispatch(signOut())
+    }
+  }
+}
+export const UserNavigation = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserNavigationComponent)
