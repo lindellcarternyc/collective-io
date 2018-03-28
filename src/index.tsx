4634f17ed877c9f176fe49f6372b5e91c0056e30
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-28 13:54:23 -0400

    added Root component and redux

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,22 @@ import './index.css'
 
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Store } from 'redux'
+import { initStore } from './redux/store'
+import { StoreState } from './redux/state'
+import { Provider } from 'react-redux'
 
+const Root = (props: { store: Store<StoreState>}) => (
+  <Provider store={props.store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+)
+
+const store = initStore()
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Root store={store}/>,
   document.getElementById('root') as HTMLElement
 )
 registerServiceWorker()
