028aef87a2bcc278e59e5582d3f82d8336b6efd6
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-29 12:14:59 -0400

    Added User class

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -5,3 +5,32 @@ export interface UserType {
   firstName: string
   lastName: string
 }
+
+export class User implements UserType {
+  readonly id: string
+  readonly email: string
+  readonly password: string
+  readonly firstName: string
+  readonly lastName: string
+
+  constructor(
+    args: UserType
+  ) { 
+    const { 
+      id, email, password, firstName, lastName
+    } = args
+    this.id = id
+    this.email = email
+    this.password = password
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+
+  get fullname(): string {
+    return this.firstName + ' ' + this.lastName
+  }
+
+  get initials(): string {
+    return this.firstName.charAt(0) + this.lastName.charAt(0)
+  }
+}
