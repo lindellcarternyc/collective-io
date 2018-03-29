8b1fb0818552602a33b514012a22620deefb020b
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-29 12:15:20 -0400

    Replaced names with IDs

--- src/data/upcoming-performances.ts
+++ src/data/upcoming-performances.ts
@@ -8,7 +8,8 @@ const MOCK_PERFORMANCE_LIST_DATA: PerformanceDetailsObject[] = [
     endTime: '10:00',
     location: 'Times Square',
     cast: [
-      'Rachel', 'Jose', 'Alexis', 'Jonathan'
+      // 'Rachel', 'Jose', 'Alexis', 'Jonathan'
+      'a', 'b', 'c', 'd'
     ]
   },
   {
@@ -16,28 +17,43 @@ const MOCK_PERFORMANCE_LIST_DATA: PerformanceDetailsObject[] = [
     date: 'Tuesday 3/27',
     startTime: '6:00',
     endTime: '9:00',
-    location: 'Union Square'
+    location: 'Union Square',
+    cast: [
+      'c', 'd', 'e', 'f'
+    ]
   },
   {
     id: 'c',
     date: 'Wednesday 3/28',
     startTime: '6:00',
     endTime: '9:00',
-    location: 'Herald Square'
+    location: 'Herald Square',
+    cast: [
+      'b', 'g', 'h'
+    ]
   },
   {
     id: 'd',
     date: 'Monday 4/02',
     startTime: '6:00',
     endTime: '9:00',
-    location: 'Port Authority'
+    location: 'Port Authority',
+    cast: [
+      'e', 'i', 'j', 'k'
+    ],
+    covers: [
+      'l'
+    ]
   },
   {
     id: 'e',
     date: 'Wednesday 4/04',
     startTime: '6:00',
     endTime: '9:00',
-    location: 'Herald Square'
+    location: 'Herald Square',
+    cast: [
+      'c', 'e', 'j', 'm'
+    ]
   }
 ]
 
