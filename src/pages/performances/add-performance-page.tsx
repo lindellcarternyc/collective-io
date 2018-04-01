9e27f48bd095cee5e15b216d8d2770b25ee49be1
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-31 23:28:16 -0400

    updated page with actions and router

--- src/pages/performances/add-performance-page.tsx
+++ src/pages/performances/add-performance-page.tsx
@@ -1,15 +1,45 @@
 import * as React from 'react'
 
+import { connect } from 'react-redux'
+import { StoreState } from '../../redux/state'
+import { Dispatch } from 'redux'
+import { addPerformance } from '../../redux/performances/actions'
+
+import { withRouter, RouteComponentProps } from 'react-router-dom'
+
 import { Layout } from '../../components/Layout'
 import { AddPerformanceForm, AddPerformanceFormData } from '../../components/add-performance/form'
 
-export const AddPerformancePage = (): JSX.Element => {
-  const addPerformance = (data: AddPerformanceFormData) => {
-    console.dir(data)
-  }
+interface AddPerformancePageComponentProps extends RouteComponentProps<{}> {
+  addPerformance: (data: AddPerformanceFormData) => void
+}
+export const AddPerformancePageComponent = (props: AddPerformancePageComponentProps): JSX.Element => {
   return (
     <Layout title="Add Performance">
-      <AddPerformanceForm addPerformance={addPerformance}/>
+      <AddPerformanceForm 
+        addPerformance={(data: AddPerformanceFormData) => {
+          props.addPerformance(data)
+          props.history.push('/performances')
+        }}
+      />
     </Layout>
   )
 }
+
+interface DispatchProps {
+  addPerformance: (data: AddPerformanceFormData) => void
+}
+const mapDispatchToProps = (dispatch: Dispatch<StoreState>): DispatchProps => {
+  return {
+    addPerformance: (data: AddPerformanceFormData) => {
+      const { date, location, startTime, endTime } = data
+      dispatch(addPerformance(date, location, startTime, endTime))
+    }
+  }
+}
+export const AddPerformancePage = withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(AddPerformancePageComponent)
+)
